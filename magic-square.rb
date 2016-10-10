3acc65a3170686e6571facff86cf9aa67fdd8c42
Claus Lensbøl <cmol@cmol.dk>
2016-10-10 14:41:19 +0200

    Making the formatting nicer

--- magic-square.rb
+++ magic-square.rb
@@ -2,81 +2,81 @@
 
 # Makes a multidimension array (matrix) with the width and height of n
 def define_matrix(n)
-	@square = Array.new(n) { Array.new(n).fill(0) }
-	@size = n
-	@max = @size * @size
+  @square = Array.new(n) { Array.new(n).fill(0) }
+  @size = n
+  @max = @size * @size
 end
 
 # Writes to the matrix
 def input(i, j, input)
-	if @square[j-1][i-1] == 0
-		@square[j-1][i-1] = input
-	end
-	#puts "Write #{input} @ array position #{j-1}, #{i-1}"
+  if @square[j-1][i-1] == 0
+    @square[j-1][i-1] = input
+  end
+  #puts "Write #{input} @ array position #{j-1}, #{i-1}"
 end
 
 # Tests if the place in the matrix is free
 def is_zero(x, y)
-	@square[y-1][x-1] == 0 && y > 0 && x > 0
+  @square[y-1][x-1] == 0 && y > 0 && x > 0
 end
 
 # Finds the next place to write
 def next_pos
-	x = @pos[0]
-	y = @pos[1]
+  x = @pos[0]
+  y = @pos[1]
 
-	if is_zero(x+1, y-1)
-		@pos = [x+1, y-1]
-	elsif x == @size && is_zero(1, y-1)
-		@pos = [1, y-1]
-	elsif y == 1 && is_zero(x+1, @size)
-		@pos = [x+1, @size]
-	elsif is_zero(x, y+1)
-		@pos = [x, y+1]
-	else
-		unless @num == @max
-			puts "Rule error!!!"
-			puts "X: #{x}"
-			puts "Y: #{y}"
-			output
-			exit
-		end
-	end
+  if is_zero(x+1, y-1)
+    @pos = [x+1, y-1]
+  elsif x == @size && is_zero(1, y-1)
+    @pos = [1, y-1]
+  elsif y == 1 && is_zero(x+1, @size)
+    @pos = [x+1, @size]
+  elsif is_zero(x, y+1)
+    @pos = [x, y+1]
+  else
+    unless @num == @max
+      puts "Rule error!!!"
+      puts "X: #{x}"
+      puts "Y: #{y}"
+      output
+      exit
+    end
+  end
 end
 
 # Finds the start position
 def start_pos
-	@pos = [(@size.to_f/2+0.5).to_i, 1]
+  @pos = [(@size.to_f/2+0.5).to_i, 1]
 end
 
 # Prints the table nicely
 def output
-	out = ""
-	@square.each do | line |
-		line.each do | number |
-		out << number.to_s.rjust(@max.to_s.length + 1)
-		end
-	out << "\n"
-	end
-	puts out
+  out = ""
+  @square.each do | line |
+    line.each do | number |
+      out << number.to_s.rjust(@max.to_s.length + 1)
+    end
+    out << "\n"
+  end
+  puts out
 end
 
 #================ Running the program ===============
 
 if $0 == __FILE__
-	if ARGV.length > 0 && (ARGV[0].to_i % 2 == 1)
-		define_matrix(ARGV[0].to_i)
-		start_pos
+  if ARGV.length > 0 && (ARGV[0].to_i % 2 == 1)
+    define_matrix(ARGV[0].to_i)
+    start_pos
 
-		for k in 1..@max
-			input(@pos[0], @pos[1], k)
-			unless k == @max
-				next_pos
-			end
-		end
+    for k in 1..@max
+      input(@pos[0], @pos[1], k)
+      unless k == @max
+        next_pos
+      end
+    end
 
-		output
-	else
-		puts "Call with an odd number!"
-	end
+    output
+  else
+    puts "Call with an odd number!"
+  end
 end
